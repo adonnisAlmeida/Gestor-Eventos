89e882653f43c67cdac58deac803a4548aad4a5f
Yerandy Reyes <yerandyreyes1985@gmail.com>
2021-10-07 11:46:27 -0400

    Signed-off-by: Yerandy Reyes <yerandyreyes1985@gmail.com>

--- event_sale_uclv/__manifest__.py
+++ event_sale_uclv/__manifest__.py
@@ -11,7 +11,9 @@
     'depends': ['event_sale', 'base_uclv'],
     'data': [
         #'views/event_registration_payment_views.xml',
-        #'views/event_registration_views.xml',
+        'views/event_ticket_views.xml',
+        'views/event_views.xml',
+        'views/event_registration_views.xml',
         'views/res_partner_views.xml',
         'report/event_event_templates.xml'
     ],
