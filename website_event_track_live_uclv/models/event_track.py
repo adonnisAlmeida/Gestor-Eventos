52ed7f72b131603b9a614b32a2ad105fefcd8bcc
Yerandy Reyes <yerandyreyes1985@gmail.com>
2021-10-29 21:45:54 -0400

    Signed-off-by: Yerandy Reyes <yerandyreyes1985@gmail.com>

--- website_event_track_live_uclv/models/event_track.py
+++ website_event_track_live_uclv/models/event_track.py
@@ -8,8 +8,12 @@ class Track(models.Model):
     _inherit = 'event.track'    
     
     def _avideo_base_domain(self):
-        self.avideo_base_domain = self.env['ir.config_parameter'].sudo().get_param('avideo.base.domain')
-        self.avideo_full_url = self.avideo_base_domain +'video/'+self.avideo_url
+        if self.avideo_url:
+            self.avideo_base_domain = self.env['ir.config_parameter'].sudo().get_param('avideo.base.domain')
+            self.avideo_full_url = self.avideo_base_domain +'video/'+self.avideo_url
+        else:
+            self.avideo_base_domain = ""
+            self.avideo_full_url = ""
 
     avideo_base_domain = fields.Char('AVideo URL', compute="_avideo_base_domain")
     avideo_url = fields.Char('AVideo URL')
