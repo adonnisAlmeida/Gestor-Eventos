5f8c2fcbb996a189c5f90c8129959e05914f64a5
Yerandy Reyes <yerandyreyes1985@gmail.com>
2022-02-16 12:03:25 -0500

    Signed-off-by: Yerandy Reyes <yerandyreyes1985@gmail.com>

---
+++ theme_siq/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'SIQ Theme',
+    'description': 'Clean and sharp design.',
+    'category': 'Theme/Corporate',
+    'summary': 'Chemistry',
+    'sequence': 110,
+    'version': '1.0.0',
+    'author': 'Yerandy Reyes',
+    'depends': ['website'],
+    'data': [
+        'views/assets.xml',
+        'views/images.xml',
+        'views/customizations.xml',
+    ],
+    'images': [
+        'static/description/siq_poster.jpg',
+        'static/description/siq_screenshot.jpg',
+    ],
+    'license': 'LGPL-3',
+    'live_test_url': 'https://siq.uclv.cu'
+}
